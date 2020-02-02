1d71b500c87a768bf83f46d84ba09c152b1f9c28
evgeniynokalevich <zplacesound@gmail.com>
2020-02-02 22:33:22 +0300

    update headers links

--- src/Controller/ProjectsController.php
+++ src/Controller/ProjectsController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProjectsController extends AbstractController
 {
     /**
-     * @Route("/projects", name="progects")
+     * @Route("/projects", name="projects")
      */
     public function index()
     {
