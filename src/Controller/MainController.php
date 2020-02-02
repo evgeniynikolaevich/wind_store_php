35b88e566874817d9a25e48488e81dd5e7af1e48
evgeniynikolaevich <zplacesound@gmail.com>
2020-02-02 12:58:12 +0300

    main with template

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,13 +2,21 @@
 
 namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
-class MainController
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+
+class MainController extends AbstractController
 {   
+    /**
+     * @Route("/")
+     */
 
     public  function showAction()
+
     {
-     return new Response("Under sea");    
+        $number = 1;
+        return $this->render('base/base.html.twig',['value'=>$number]);   
     }
 }
 ?>
