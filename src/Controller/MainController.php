87eedd0b9ea73671c95ba6135ee1c1872fafb140
evgeniynokalevich <zplacesound@gmail.com>
2020-02-06 12:54:51 +0300

    initial commit begin at start to fix

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -1,22 +1,25 @@
-<?php 
+<?php
 
 namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+ use App\Repository\WindGeneratorRepository;
 
 
 class MainController extends AbstractController
-{   
+{
     /**
      * @Route("/", name="main")
      */
 
-    public  function showAction()
+    public  function showAction(WindGeneratorRepository $rep)
 
     {
+
         $number = 1;
-        return $this->render('main/main.html.twig',['res'=>$number]);   
+        $generators = $rep->findAll();
+        return $this->render('main/main.html.twig',['res'=>$number,'generators'=> $generators]);
     }
 }
 ?>
