a8e010affdbeacd6fca776393f101055e55ebe7f
evgeniynokalevich <zplacesound@gmail.com>
2020-02-10 16:48:25 +0300

    from merge

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+ use App\Repository\WindGeneratorRepository;
 
 
 class MainController extends AbstractController
@@ -12,7 +13,7 @@ class MainController extends AbstractController
      * @Route("/", name="main")
      */
 
-    public  function showAction()
+    public  function showAction(WindGeneratorRepository $rep)
 
     {
 
