39fb9a42bfeca4d69ea0a3fcc2dc8ebb41e9b528
evgeniy gazetdinov <zplacesound@gmail.com>
2020-02-13 21:50:06 +0300

    changes for login (#11)

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -4,6 +4,60 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use App\Entity\Generator;
+use Symfony\Component\HttpFoundation\Request;
+/*
+class CartForController
+{
+  private $session;
+
+  public function delete_from_cart(Sea)
+  {
+    $this->cart();
+
+  }
+
+
+  private function start_shopping()
+  {
+    $session = new Session();
+    $session->start();
+    // $session->set('name', 'Drak');
+    // $session->get('name');
+  }
+  private function continue_shopping()
+  {
+    $user_session_id = 1;
+    $this->session = $user_session_id;
+
+  }
+
+
+
+  public function cart_start()
+  {
+    if(!user->session)
+    {
+      $this->start_shopping();
+    }
+  }
+
+  public function __construct()
+  {
+    //check if user has bought
+    //return session
+
+      $this->current_user = 1#db manipulation check user session
+      $this->cart_start();
+
+  }
+
+
+}
+
+*/
+
 
 class CartController extends AbstractController
 {
@@ -14,6 +68,20 @@ class CartController extends AbstractController
     {
         return $this->render('cart/index.html.twig', [
             'controller_name' => 'CartController',
+        ]);
+    }
+    /**
+     * @Route("/cart/", name="add_to_cart",methods ="POST")
+     */
+    public function add_to_cart(int $id)
+    { #get generator by id
+      #push to session user this stuff
+      #redirect to cart
+
+        return $this->render('cart/index.html.twig', [
+            'title' => 'Корзина',
+            'controller_name' => 'CartController',
+            'is' => $id
         ]);
     }
 }
---
+++ src/Migrations/Version20200213181027.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200213181027 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE wind_generator (id INT AUTO_INCREMENT NOT NULL, power INT NOT NULL, voltage INT NOT NULL, type VARCHAR(255) NOT NULL, discription LONGTEXT NOT NULL, generator_name VARCHAR(255) NOT NULL, projects VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE user CHANGE password session VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE wind_generator');
+        $this->addSql('ALTER TABLE user CHANGE session password VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
+    }
+}
